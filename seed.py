3bf174653884f2529d5ccd24771a5fd36910cfe7
Sebastian <Setho2104@oslomet.no>
2026-07-30 16:50:58 +0200

    feat: connect to database and run schema

--- seed.py
+++ seed.py
@@ -1,3 +1,7 @@
 import sqlite3
 
-conn = sqlite3.connect("flytoget.db")
+conn = sqlite3.connect("flytoget.db") # kobler til og lager (om det ikke finnes) database
+cur = conn.cursor() # gjør det mulig å kjøre SQL gjennom tilkoblingen
+
+with open("schema.sql") as f:
+    cur.executescript(f.read()) # les all teksten fra filen, og kjør sql setningene i den
