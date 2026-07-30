300577b536f63ab66c51871ae254a5b3bb92b994
Sebastian <Setho2104@oslomet.no>
2026-07-30 17:01:57 +0200

    feat: add os logic to remove database if existing

--- seed.py
+++ seed.py
@@ -1,6 +1,12 @@
+import os
 import sqlite3
 
-conn = sqlite3.connect("flytoget.db") # kobler til og lager (om det ikke finnes) database
+DB_FILE = "flytoget.db"
+
+if os.path.exists(DB_FILE): # sjekker og sletter flytoget.db om den finnes
+    os.remove(DB_FILE)
+
+conn = sqlite3.connect(DB_FILE) # kobler til og lager (om det ikke finnes) database
 cur = conn.cursor() # gjør det mulig å kjøre SQL gjennom tilkoblingen
 
 with open("schema.sql") as f:
