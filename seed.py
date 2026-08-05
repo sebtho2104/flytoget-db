22d60c8b885c688b1803ace50d7cb0679e3f39d6
Sebastian <Setho2104@oslomet.no>
2026-08-05 14:14:25 +0200

    feat: seed stasjoner and add commit/close

--- seed.py
+++ seed.py
@@ -11,3 +11,21 @@ cur = conn.cursor() # gjør det mulig å kjøre SQL gjennom tilkoblingen
 
 with open("schema.sql") as f:
     cur.executescript(f.read()) # les all teksten fra filen, og kjør sql setningene i den
+
+stasjoner = [
+    (1, "Gardemoen"),
+    (2, "Lillestrøm"),
+    (3, "Oslo s"),
+    (4, "Nationaltheatret"),
+    (5, "Skøyen"),
+    (6, "Stabekk"),
+    (7, "Lysaker"),
+    (8, "Sandvika"),
+    (9, "Asker"),
+    (10, "Drammen")
+]
+
+cur.executemany("INSERT INTO stasjoner (stasjons_id, navn) VALUES (?, ?)", stasjoner)
+
+conn.commit()
+conn.close()
